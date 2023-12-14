eebe511084afdcec1475adc1ca01cb478358f81a
admin <dsfasdafes@asdas.sd>
2023-12-14 15:01:48 +0400

    commit

--- main.py
+++ main.py
@@ -14,11 +14,13 @@ session.commit()
 
 @app.route('/new_signal', methods=['POST'])
 def new_signal():
+    res = {'status': 'ok'}
     state_yellow, state_red = request.json['first'], request.json['second']
     q_state = session.get(QueryState, 1)
     q_state.state_yellow = state_yellow
     q_state.state_red = state_red
     session.commit()
+    return jsonify(res)
 
 
 @app.route('/get_state', methods=['GET'])
